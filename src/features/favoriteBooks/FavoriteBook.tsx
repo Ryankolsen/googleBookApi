527f7695636456e4e0f0090fbd7ac76a1a335f3f
Ryan Olsen <ryankolsen@gmail.com>
2022-04-12 21:58:12 -0400

    added spinner and error logic to FavoriteBook

--- src/features/favoriteBooks/FavoriteBook.tsx
+++ src/features/favoriteBooks/FavoriteBook.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useParams } from "react-router-dom";
 import { useGetBookByIDQuery } from "../books/booksSlice";
 import { Card } from "react-bootstrap";
+import { Spinner } from "../../components/Spinner";
 
 export const FavoriteBook = () => {
   const bookId: any = useParams().bookId?.toString();
@@ -9,15 +10,21 @@ export const FavoriteBook = () => {
 
   return (
     <>
-      <Card>
-        <Card.Header className="book__Card-Header">
-          <h1>{data?.volumeInfo.title}</h1>
-        </Card.Header>
-        <Card.Body>
-          <h4>Author:{data?.volumeInfo.authors.map((author) => author)}</h4>
-          <p>{data?.volumeInfo.description}</p>
-        </Card.Body>
-      </Card>
+      {error ? (
+        <>An Error Occurred!</>
+      ) : isLoading ? (
+        <Spinner />
+      ) : data ? (
+        <Card>
+          <Card.Header className="book__Card-Header">
+            <h1>{data?.volumeInfo.title}</h1>
+          </Card.Header>
+          <Card.Body>
+            <h4>Author:{data?.volumeInfo.authors.map((author) => author)}</h4>
+            <p>{data?.volumeInfo.description}</p>
+          </Card.Body>
+        </Card>
+      ) : null}
     </>
   );
 };
